a0b9a0e83ef7136a5072fe1871f426796c2538ea
kjlee772 <kjlee50612@gmail.com>
2021-10-30 22:29:26 +0900

    first commit

---
+++ server/models/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const path = require('path');
+const Sequelize = require('sequelize');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(path.join(__dirname, '..', 'config', 'db.json'))[
+  env
+];
+const db = {};
+
+let sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config,
+  {
+    defiine: {
+      charset: 'utf8',
+      collate: 'utf9_general_ci'
+    }
+  }
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.sequelize
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.log('Unable to connect to the database: ', err);
+});
+
+
+db.Teacher = require('./teacher')(sequelize, Sequelize);
+db.Class = require('./class')(sequelize, Sequelize);
+
+
+
+db.secret = '(9*)5$&!3%^0%^@@2$1!#5@2!4';
+module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,69 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Response Complete!');
+const bodyParser = require('body-parser');
+
+const sequelize = require('./models').sequelize;
+sequelize.sync();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const {
+  Teacher,
+  Sequelize: { Op }
+} = require('./models');
+sequelize.query('SET NAMES utf8;');
+
+app.post('/add/data', (req, res) => {
+  console.log(req.body);
+  Teacher.create({
+    name: req.body.data
+  })
+    .then(result => {
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    })
+})
+
+app.get('/get/data', (req, res) => {
+  Teacher.findAll()
+    .then(result => { res.send(result) })
+    .catch(err => { throw err })
+})
+
+app.post('/modify/data', (req, res) => {
+  Teacher.update({ name: req.body.modify.name }, {
+    where: { id: req.body.modify.id }
+  })
+    .then(result => { res.send(result) })
+    .catch(err => { throw err })
+})
+
+app.post('/delete/data', (req, res) => {
+  Teacher.destroy({
+    where: { id: req.body.delete.id }
+  })
+    .then(res.sendStatus(200))
+    .then(Teacher.Update({
+      
+    }))
+    .catch(err => { throw err })
 })
 
 
+
+
+
+app.get('/api/host', (req, res) => {
+  res.send({ host: 'kjlee' });
+})
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server On : http://localhost:${PORT}/`);
 })
